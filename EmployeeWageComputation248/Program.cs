c67c11883c949c7fa9d5b84894b541b3bb63ae91
Virtapatil2000 <virtapatil01@gmail.com>
2023-01-20 22:27:06 +0530

    [Virta] Modified Calculating Employee Attendence

--- EmployeeWageComputation248/Program.cs
+++ EmployeeWageComputation248/Program.cs
@@ -4,8 +4,20 @@
     {
         static void Main(string[] args)
         {
+            int isPresent = 1;
             Console.WriteLine("Welcome Employee Wage Computation");
-            Console.WriteLine();
+            //UC1- EmployeeAttendence
+            Random random = new Random();
+            int empAttendence = random.Next(0, 2);//0 or 1
+            if (empAttendence == isPresent)
+            {
+                Console.WriteLine("Emloyee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            Console.ReadLine();
         }
     }
 }
