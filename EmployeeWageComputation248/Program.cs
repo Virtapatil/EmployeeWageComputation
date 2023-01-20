5aa9e8572b3899b1bbf48dc89205be4957a8cd25
Virtapatil2000 <virtapatil01@gmail.com>
2023-01-20 22:16:04 +0530

    [Virta] Employee Wage Computation

---
+++ EmployeeWageComputation248/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeWageComputation248
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Employee Wage Computation");
+            Console.WriteLine();
+        }
+    }
+}
