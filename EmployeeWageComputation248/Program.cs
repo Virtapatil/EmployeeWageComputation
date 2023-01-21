254c01b702da8361cd3bb4c4c5e9320fd3efca2e
Virtapatil2000 <virtapatil01@gmail.com>
2023-01-21 21:50:00 +0530

    [Virta] Modifing Switch Case Employee Wage

--- EmployeeWageComputation248/Program.cs
+++ EmployeeWageComputation248/Program.cs
@@ -10,25 +10,23 @@ namespace EmployeeWageComputation248
             const int PART_TIME = 2;
             int EMP_RATE_PER_HR = 20;
             int empHrs = 0, empWage = 0;
-
+            
             Console.WriteLine("Welcome Employee Wage Computation");
             //UC1- EmployeeAttendence
             Random random = new Random();
             int empAttendence = random.Next(0, 3);//0 or 1
-            if (empAttendence == FULL_TIME)
+            switch (empAttendence)
             {
-                Console.WriteLine("full Emloyee is Present");
-                empHrs = 8;
-            }
-            else if (empAttendence==PART_TIME)
-            {
-                Console.WriteLine("Part Emloyee is Present");
-                empHrs = 4;
-            }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-                empHrs = 0;
+                case FULL_TIME:
+                    Console.WriteLine("Full Employee is Present");
+                    empHrs = 8;
+                    break;
+                case PART_TIME:
+                    Console.WriteLine("Part Employee is Present");
+                    empHrs = 4;
+                    break;
+                default:
+                    break;
             }
             empWage = empHrs * EMP_RATE_PER_HR;
             //Console.WriteLine("Emplyee" + EmpWage + "Wage:");
