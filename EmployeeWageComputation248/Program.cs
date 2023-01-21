7a9c517692a64a61bd32167644e5779b911778f1
Virtapatil2000 <virtapatil01@gmail.com>
2023-01-21 09:14:26 +0530

    [Virta] modifing Part Time EmployeeWage

--- EmployeeWageComputation248/Program.cs
+++ EmployeeWageComputation248/Program.cs
@@ -7,17 +7,23 @@ namespace EmployeeWageComputation248
         static void Main(string[] args)
         {
             const int FULL_TIME = 1;
+            const int PART_TIME = 2;
             int EMP_RATE_PER_HR = 20;
             int empHrs = 0, empWage = 0;
 
             Console.WriteLine("Welcome Employee Wage Computation");
             //UC1- EmployeeAttendence
             Random random = new Random();
-            int empAttendence = random.Next(0, 2);//0 or 1
+            int empAttendence = random.Next(0, 3);//0 or 1
             if (empAttendence == FULL_TIME)
             {
-                Console.WriteLine("Emloyee is Present");
+                Console.WriteLine("full Emloyee is Present");
                 empHrs = 8;
+            }
+            else if (empAttendence==PART_TIME)
+            {
+                Console.WriteLine("Part Emloyee is Present");
+                empHrs = 4;
             }
             else
             {
